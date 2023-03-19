e26abd6de66ef110ca4bac4a193fb9660a07c324
FahriAs <fahriandriansaputra123@gmail.com>
2023-03-19 16:18:21 +0700

    k

---
+++ plugins/virus-list.js
@@ -0,0 +1,77 @@
+let handler = async (m, { conn }) => {
+let info = `
+*${htki} VIRUS ${htka}*
+          
+ 🔥BY ᴹᴿ᭄ PutraModz ×፝֟͜×🔥
+          
+                  
+–––––– *ᴋᴇʙɪᴊᴀᴋᴀɴ* ––––––
+*📮Gunakan Dengan tujuan yang benar. Jangan untuk merugikan orang lain tanpa alasan!*
+`
+const sections = [
+   {
+    title: `✃ VIRTEX`,
+	rows: [
+	    {title: "😈Virtex 1", rowId: '.virtex1', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	    {title: "😈Virtex 2", rowId: '.virtex2', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 3", rowId: '.virtex3', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 4", rowId: '.virtex4', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 5", rowId: '.virtex5', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 6", rowId: '.virtex6', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Virtex 7", rowId: '.virtex7', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 8", rowId: '.virtex8', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 9", rowId: '.virtex9', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 10", rowId: '.virtex10', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 11", rowId: '.virtex11', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 12", rowId: '.virtex12', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 13", rowId: '.virtex13', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 14", rowId: '.virtex14', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Virtex 15", rowId: '.virtex15', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 16", rowId: '.virtex16', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 17", rowId: '.virtex17', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 18", rowId: '.virtex18', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	{title: "😈Virtex 19", rowId: '.virtex19', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Virtex 20", rowId: '.virtex20', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Bugpoll", rowId: '.bugpoll', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Inibug", rowId: '.inibug', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Bugput", rowId: '.arifbug', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Santetput", rowId: '.rifsantet', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Putdarknes", rowId: '.rifdarknes', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    {title: "😈Sendarknes", rowId: '.senddarknes', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+     {title: "😈Putcrash", rowId: '.rifcrash', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+     {title: "😈Sandcrash", rowId: '.sendcrash', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+     {title: "😈Putstick", rowId: '.rifstick', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+     {title: "😈Kanjut", rowId: '.kanjut', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+    
+	]
+    }, {
+    title: `✃ INFO`,
+	rows: [
+	    {title: "🔥Download WhatsApp Imune", rowId: '.waantivirus', description: 'Download WhatsApp Kebal' },
+	    {title: "🔥Virus Troli", rowId: '.troli', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	    {title: "🔥Virus Power", rowId: '.listbuggc', description: 'Jangan lupa Subs Yt ᴹᴿ᭄ PutraModz ×፝֟͜× Offcial' },
+	    ]
+        }, {
+    title: `✃ BUY`,
+	rows: [
+	    {title: "💸Upgrade Premium", rowId: '.sewa', description: 'Dan unclock Fitur premium lainya!' },
+	    ]
+        },
+]
+
+const listMessage = {
+  text: ' ',
+  footer: info,
+  title: null,
+  buttonText: "👺GASS",
+  sections
+}
+await conn.sendMessage(m.chat, listMessage, { quoted: m})
+//conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+}
+handler.help = ['virtex']
+handler.tags = ['virus']
+handler.command = /^virtex$/i
+handler.premium = true
+
+export default handler
